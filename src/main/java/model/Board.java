43cdeb15c0ca3bae0e0413b8c14a1cfff4f3eaee
JuanEdgarBaldelomar <>
2020-10-20 18:13:18 +0200

    metodoSizeTableDone

--- src/main/java/model/Board.java
+++ src/main/java/model/Board.java
@@ -1,5 +1,17 @@
 package model;
 
 public class Board {
-
+	
+	private static final int sizeX  = 10;
+	private static final int sizeY = 10;
+	
+	
+	public int getSizeX() {
+		return sizeX;
+	}
+	
+	public int getSizeY() {
+		return sizeY;
+	}
+	
 }
