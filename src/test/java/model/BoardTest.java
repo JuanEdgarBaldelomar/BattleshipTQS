756a3413833bae9148a3637e3d02721f267a6c30
JuanEdgarBaldelomar <>
2020-10-20 13:59:43 +0200

    testSizeBoardDone

---
+++ src/test/java/model/BoardTest.java
@@ -0,0 +1,22 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BoardTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	public void testTableSizes() {
+		
+		Board gameBoard = new Board();
+		
+		assertTrue(gameBoard.getSizeX()==10);
+		assertTrue(gameBoard.getSizeY()==10);
+	}
+
+}
