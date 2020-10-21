57290ef1a215b781e524a56cca30340056998aab
JuanEdgarBaldelomar <>
2020-10-21 13:47:25 +0200

    constructor Board y metodo equals

--- src/main/java/model/Board.java
+++ src/main/java/model/Board.java
@@ -1,17 +1,48 @@
 package model;
 
+import java.util.Arrays;
+
+
+
 public class Board {
 	
-	private static final int sizeX  = 10;
-	private static final int sizeY = 10;
+	private static final int msizeX  = 10;
+	private static final int msizeY = 10;
+	private int[][] mBoard;
 	
+	public Board() {
+		
+		mBoard = new int[msizeX][msizeY];
+		for (int i = 0;i<msizeX;i++) {
+			for (int j = 0;j<msizeY;j++) {
+				
+				mBoard[i][j] = 0;
+				
+			}
+		}
+		
+	}
+	
+	
+	public int[][] getBoard(){
+		return mBoard;
+	}
 	
 	public int getSizeX() {
-		return sizeX;
+		return msizeX;
 	}
 	
 	public int getSizeY() {
-		return sizeY;
+		return msizeY;
 	}
 	
+	public boolean equals(Board b2) {
+		
+		if(Arrays.deepEquals(this.mBoard, b2.getBoard())) {
+			return true;
+		}else {
+			return false;
+		}
+		
+	}
 }
