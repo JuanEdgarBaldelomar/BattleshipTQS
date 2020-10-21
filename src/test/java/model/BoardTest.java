3b7255a8d6f776be50e84b559e35127490763c23
JuanEdgarBaldelomar <>
2020-10-21 13:22:02 +0200

    cambio nombre test y se añade test init tablero

--- src/test/java/model/BoardTest.java
+++ src/test/java/model/BoardTest.java
@@ -8,12 +8,15 @@ public class BoardTest {
 
 
 	@Test
-	public void testTableSizes() {
+	public void testInitBoard() {
 		
 		Board gameBoard = new Board();
-		
+		Board gameBoard2 = new Board();
 		assertTrue(gameBoard.getSizeX()==10);
 		assertTrue(gameBoard.getSizeY()==10);
+		assertTrue(gameBoard.equals(gameBoard2));
 	}
+	
+	
 
 }
