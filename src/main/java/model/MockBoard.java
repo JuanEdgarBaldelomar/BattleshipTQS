dae5bf8db1be15fa8f4a263ad694af56cbc062a0
JuanEdgarBaldelomar <>
2020-11-12 00:50:11 +0100

    implementacion mockboard

---
+++ src/main/java/model/MockBoard.java
@@ -0,0 +1,112 @@
+package model;
+
+public class MockBoard extends Board {
+
+	public MockBoard() {
+		super();
+	}
+	
+	
+	
+	public int[][] getTestBoardLittleBoats(){
+		
+		int[][] board = getBoard();
+		System.out.println(board);
+		board[0][1] = 1;
+		board[0][8] = 1;
+		board[1][4] = 1;
+		board[2][4] = 1;
+		board[4][6] = 1;
+		board[4][9] = 1;
+		board[7][6] = 1;
+		board[7][9] = 1;
+		
+		return board;
+	}
+	
+	public int[][] getTestMediumBoats() {
+		int[][] board = getBoard();
+		
+		board[0][2] = 1;
+		board[0][7] = 1;
+		board[2][2] = 1;
+		board[6][2] = 1;
+		board[6][6] = 1;
+		board[6][7] = 1;
+		board[7][4] = 1;
+		
+		return board;
+	}
+	
+	
+	public int[][] getTestMediumBoatsDiagonal(){
+		int[][] board = getBoard();
+		
+		board[0][0] = 1;
+		board[0][9] = 1;
+		board[4][0] = 1;
+		board[4][9] = 1;
+		board[9][3] = 1;
+		board[9][7] = 1;
+		
+		return board;
+	
+	}
+	
+	public int[][] getgetTestBigBoats(){
+		int[][] board = getBoard();
+		
+		board[0][3] = 1;
+		board[0][6] = 1;
+		board[2][7] = 1;
+		board[3][3] = 1;
+		board[6][8] = 1;
+		board[7][1] = 1;
+		board[9][7] = 1;
+		
+		return board;
+	}
+	
+	public int[][] getgetTestBigBoatsDiagonal(){
+		int[][] board = getBoard();
+		
+		board[1][0] = 1;
+		board[1][9] = 1;
+		board[6][7] = 1;
+		
+		return board;
+	}
+	
+	public int[][] getgetTestSubmarine(){
+		
+		int[][] board = getBoard();
+		
+		board[0][4] = 1;
+		board[3][4] = 1;
+		board[5][1] = 1;
+		board[6][8] = 1;
+		board[9][4] = 1;
+		
+		return board;
+		
+	}
+	
+	public int[][] getgetTestSubmarineDiagonal(){
+		
+		int[][] board = getBoard();
+		
+		board[1][5] = 1;
+		board[5][5] = 1;
+		board[5][9] = 1;
+		board[2][9] = 1;
+		
+		return board;
+		
+		
+	}
+	
+	
+	
+	
+}
+
